36a032fbe63745d5aae934ce7acd5fa6f9ac48e9
Viktor Korolko <vikingru3825@gmail.com>
2023-08-22 14:28:57 +0400

    transfer method createNewPaste method to NewPasteFage class from Page class

--- pages/newpaste.js
+++ pages/newpaste.js
@@ -35,5 +35,17 @@ class NewPastePage extends HomePage {
     open() {
         return super.open("https://pastebin.com/");
     }
+    async createNewPaste(testData) {
+        await this.newPasteField.setValue(testData.text);
+        await this.adCloseButton.click(); // close Advertisment Bunner          
+        if (testData.syntax) {
+            await this.checkBoxSyntaxHighlight.click();        
+            await this.dropDownMenuHighlght.click();
+            await this.valueBashHighlghtMenu.click(); 
+        };
+        await this.dropDownMenuExpiration.click();
+        await this.value10MinExpirationMenu.click();               
+        await this.titleField.setValue(testData.title);
+    }
 }
 module.exports = NewPastePage;
--- pages/page.js
+++ pages/page.js
@@ -2,17 +2,6 @@ class HomePage {
     async open(path) {
         await browser.url(path);
     }
-    async createNewPaste(testData) {
-        await this.newPasteField.setValue(testData.text);
-        await this.adCloseButton.click(); // close Advertisment Bunner          
-        if (testData.syntax) {
-            await this.checkBoxSyntaxHighlight.click();        
-            await this.dropDownMenuHighlght.click();
-            await this.valueBashHighlghtMenu.click(); 
-        };
-        await this.dropDownMenuExpiration.click();
-        await this.value10MinExpirationMenu.click();               
-        await this.titleField.setValue(testData.title);
-    }
+    
 }
-module.exports=HomePage;
+module.exports = HomePage;
